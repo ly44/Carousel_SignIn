570ab6b31d0b12b366cb8b2eca642f7d824040f3
Lin Yang <lininthehand@gmail.com>
2015-11-05 13:28:21 -0800

    Update
    Fixed sign-in keyboard

--- Carousel_SignIn/LoginViewController.swift
+++ Carousel_SignIn/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UIScrollViewDelegate {
     // defining scrollview variables
     @IBOutlet weak var loginScrollView: UIScrollView!
     @IBOutlet weak var buttonParentView: UIView!
@@ -19,7 +19,8 @@ class LoginViewController: UIViewController {
     }
     
     // defining variables for image offset
-    var maxContentOffsetY: CGFloat!
+    var buttonInitialY: CGFloat!
+    var buttonOffset: CGFloat!
 
     // defining form fields
     @IBOutlet weak var emailField: UITextField!
@@ -34,6 +35,14 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // setting scrollview delegate
+        loginScrollView.delegate = self
+        loginScrollView.contentSize = loginScrollView.frame.size
+        loginScrollView.contentInset.bottom = 100
+        
+        buttonInitialY = buttonParentView.frame.origin.y
+        buttonOffset = -70
+        
         // Register keyboard events
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
@@ -48,30 +57,26 @@ class LoginViewController: UIViewController {
     
     // when the keyboard is shown
     func keyboardWillShow(notification: NSNotification) {
+        print("keyboardWillShow")
         // Move the button up above keyboard
-        buttonParentView.transform = CGAffineTransformMakeTranslation(0, -80)
-        // Calculate the maximum scrollview content Offset y
-        //let maxContentOffsetY = loginScrollView.frame.size.height - loginScrollView.contentSize.height
-        // Scroll the scrollview up to the maximum contentOffset
-        //loginScrollView.contentOffset.y = maxContentOffsetY
-        // Enable scrolling while keyboard is shown
-        //loginScrollView.scrollEnabled = true
+        buttonParentView.frame.origin.y = buttonInitialY + buttonOffset
+        // Scroll the scrollview up
+        loginScrollView.contentOffset.y = loginScrollView.contentInset.bottom
     }
     
     // when keyboard is hidden
     func keyboardWillHide(notification: NSNotification!) {
         // Move the button back when keyboard is dismissed
-        buttonParentView.transform = CGAffineTransformIdentity
-        // Return the ScrollView to it's original position
-        //loginScrollView.contentOffset.y = 0
-        // Disable scrolling when keyboard is hidden
-        //loginScrollView.scrollEnabled = false
+        buttonParentView.frame.origin.y = buttonInitialY
+        // Scroll the scrollview back down
+        loginScrollView.contentOffset.y = 0
     }
 
+
+    
     // when user scrolls -> hide keyboard
     func scrollViewDidScroll(scrollView: UIScrollView) {
-        // If the scrollView has been scrolled down by 50px or more...
-        if loginScrollView.contentOffset.y <= -50 {
+        if loginScrollView.contentOffset.y <= -10 {
             // Hide the keyboard
             view.endEditing(true)
         }
